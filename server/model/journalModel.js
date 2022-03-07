f955cdf1a450845017104ca0bd9814b373a984f1
Henry <uce.henrys@gmail.com>
2022-03-07 11:11:59 +0000

    Adding the tests for model and changing comments data section to an array

--- server/model/journalModel.js
+++ server/model/journalModel.js
@@ -1,3 +1,17 @@
-const data = require("../data/data");
+//import
+const journalData = require("../data/data");
 
 
+class Journal {
+    constructor(data) {
+        this.id = data.id;
+        this.title = data.title;
+        this.body = data.body; 
+        this.image = data.image;
+        this.comments = data.comments
+    }
+}
+
+
+
+module.exports = Journal;
