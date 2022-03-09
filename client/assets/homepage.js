b2a26e31ccc2a459fc1fceefc98a729f798686d0
Henry <uce.henrys@gmail.com>
2022-03-09 13:01:44 +0000

    cleaning up some code

--- client/assets/homepage.js
+++ client/assets/homepage.js
@@ -1,4 +1,4 @@
-
+let articleID;
 
 //pulls from database
 document.addEventListener("DOMContentLoaded", fill);
@@ -21,15 +21,12 @@ function fill() {
 
 }
 
-
-
-
-
 // put results from database into homepage 
 
 const fillPost = data => {
-
+ 
     for (let entry of data) {
+        articleID = entry.id;
 
         const article = document.createElement("article")
         // const section = document.getElementById("toShow");
@@ -125,6 +122,60 @@ const fillPost = data => {
 
     }
 }
+
+const viewComments = document.querySelector('#viewComments');
+const newComment = document.querySelector('#form');
+const commentSection = document.querySelector('.commentSection');
+
+viewComments.addEventListener('click', searchResults);
+newComment.addEventListener('submit', addingComment);
+
+function searchResults(e, id){
+    commentSection.innerHTML = ""
+    e.preventDefault();
+    id = articleID;
+    // const classes = ['card', 'card-body'];
+    // element.classList.add(...list);
+    fetch(`http://localhost:3000/data/${id}/comments`)
+        .then(res => res.json())
+        .then(res => {
+            for (const comment in res) {
+                const element = res[comment];
+                let addComments = document.createElement('div');
+                addComments.classList.add('card', 'card-body');
+                addComments.textContent = element;
+                commentSection.appendChild(addComments);
+            }
+        });
+};
+
+function addingComment(e, id){
+    e.preventDefault();
+
+    const comment = {
+        comments: e.target.message.value
+    };
+
+    console.log(comment)
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(comment),
+        headers: {
+            "Content-Type" : "application/json"
+        }
+    };
+
+    console.log(options.body);
+
+    id = articleID;
+
+    fetch(`http://localhost:3000/data/${id}/comments`, options)
+    .then(res => console.log(res.json()))
+    .catch(console.warn);
+}
+
+
 
 
 
