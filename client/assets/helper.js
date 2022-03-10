576e9b13bb81f90cdb2af9b67b654b7c6f7c223b
Tom Grainger <Tom.Luke.Grainger@gmail.com>
2022-03-10 09:00:05 +0000

    tidying up of JS start

--- client/assets/helper.js
+++ client/assets/helper.js
@@ -1,8 +1,8 @@
-let button = document.getElementById("button1"),
-count = 0;
-button.onclick = function(e) {
-    e.preventDefault()
-    count += 1;
-    button.innerHTML = count;
-    console.log(button.innerHTML)
-};
+// let button = document.getElementById("button1"),
+// count = 0;
+// button.onclick = function(e) {
+//     e.preventDefault()
+//     count += 1;
+//     button.innerHTML = count;
+//     console.log(button.innerHTML)
+// };
