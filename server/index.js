3ac193a6018aaa03b7b7b20a1954cf4b582dadcf
Henry <uce.henrys@gmail.com>
2022-03-10 15:08:38 +0000

    server test

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
-const server = require("./server")
+const server = require("./server");
 
-server.listen(3000, () => console.log("\n Express port 3000"))
+const port = process.env.PORT || 3000; // if there is no PORT env variable, 3000 will be used
+server.listen(port, () => console.log(`\n Express is running on ${port}`));
--- server/server.js
+++ server/server.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use(express.static(path.join(__dirname, '../client')));
+
 const journalRoutes = require("./controller/journalController")
-app.use('/data', journalRoutes)
+app.use('https://fpjournaling.com', journalRoutes)
+
+app.get('/', (req, res) => res.send('../client/index.html'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
